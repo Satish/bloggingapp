d5f2e2ac903f8d48229ed4ce07a746b6eb3507a0
Satish Chauhan <satishchauhan80@gmail.com>
2009-03-05 17:48:14 +0530

    added model and controllers Post, Comment, Page and plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -23,16 +24,19 @@ ActionController::Routing::Routes.draw do |map|
   #     products.resources :comments
   #     products.resources :sales, :collection => { :recent => :get }
   #   end
+  
+   map.root :controller => "posts"
+   map.resources :posts
+   map.resources :pages
+  
+   map.namespace :admin do |admin|
+      admin.root :controller => 'dashboard'		
+      admin.resources :posts
+      admin.resources :comments
+      admin.resources :pages
+   end
 
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
+  
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
